3947b1e87f312c0fd9c7b72b177007964e23f62b
dindagladies <dindagladies44@gmail.com>
2019-07-21 14:03:01 +0700

    01-02

--- novice/01-02/materi.js
+++ novice/01-02/materi.js
@@ -1,16 +1,31 @@
 # MINIMUM
 
-a = 0
-b = 0
-
-function min(a,b){
+const min = function(a,b){
 if (a<b){
-	return a;
+return a;
 }
+else if (a>b){
+	return b;
+}}
+console.log(min(0,10))
 
-else if (b<a){
-	return a
-}
-}
-console.log(min(a,b))
+#RECURSION
 
+const isEven = function(angka){
+if (angka >= 0){
+return true;
+}
+else{
+return false;}}
+
+console.log(isEven(50));
+
+#BEAN COUNTING
+const countChar(str, char){
+var count = 0;
+for (var i = 0 , i < str.length; i++){
+if (str.chartAt(i) === char){
+count++;}
+return (count);}}
+
+console.log(countChar("BBC))
